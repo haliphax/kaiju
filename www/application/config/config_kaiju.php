4395aeb67f4441c0264c8c6e59f82e6030099159
Todd Boyd <haliphax@gmail.com>
2012-03-03 12:03:23 -0600

    fixing execution permissions

--- www/application/config/config_kaiju.php
+++ www/application/config/config_kaiju.php
@@ -1,6 +1,6 @@
 <?php  if(! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['salt'] = 'wo9Z;od1&F3ch0(QucL:2k';
-$config['maintfile'] = '/home/roadhaus/vhosts/kaiju.roadha.us/maint';
+$config['maintfile'] = '/home/roadhaus/vhosts/kaiju-dev.roadha.us/maint';
 $config['version'] = '0.10.1';
 $config['codename'] = 'kouhai';
