c185729edf44c218bed0706003fd82268046eefe
Todd Boyd <haliphax@gmail.com>
2012-07-17 18:27:48 -0500

    fixed db-dump script for oddnetwork server paths and sql auth

--- tools/db-dump.sh
+++ tools/db-dump.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mypath="/home/roadhaus/vhosts/kaiju-dev.roadha.us"
+mypath="/home/haliphax/vhosts/kaiju"
 rm $mypath/db.sql
-mysqldump -uroadhaus_kaiju -pm4i1b0x \
+mysqldump -ukaiju -prGtA6sUdFaZPFQNe \
 	--ignore-table=roadhaus_kaiju_test.ci_sessions \
 	--ignore-table=roadhaus_kaiju_test.user \
 	--ignore-table=roadhaus_kaiju_test.user_mod \
@@ -16,10 +16,10 @@ mysqldump -uroadhaus_kaiju -pm4i1b0x \
 	--ignore-table=roadhaus_kaiju_test.actor_item_ammo \
 	--ignore-table=roadhaus_kaiju_test.pdata \
     --ignore-table=roadhaus_kaiju_test.event \
-    --ignore-table=roadhaus_kaiju_test.event_thread roadhaus_kaiju_test \
+    --ignore-table=roadhaus_kaiju_test.event_thread kaiju_dev \
     > $mypath/db.sql
-mysqldump -uroadhaus_kaiju -pm4i1b0x \
-	-i -R -d roadhaus_kaiju_test ci_sessions user user_mod user_priv_mapedit \
+mysqldump -ukaiju -prGtA6sUdFaZPFQNe \
+	-i -R -d kaiju_dev ci_sessions user user_mod user_priv_mapedit \
 		actor actor_item actor_effect actor_npc actor_skill actor_class \
 		actor_item_ammo pdata event event_thread \
 	>> $mypath/db.sql
