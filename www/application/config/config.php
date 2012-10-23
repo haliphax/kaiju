efe57bc2b826192fdf3be6d498b21401f40634d0
Todd Boyd <haliphax@gmail.com>
2012-10-23 09:45:53 -0500

    QA URL

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://kaijugame.phpfogapp.com/';
+$config['base_url']	= 'https://qa-kaijugame.phpfogapp.com/';
 
 /*
 |--------------------------------------------------------------------------
