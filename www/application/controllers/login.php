8793b181e8e216d4751edf9312306b50df14750f
Todd Boyd <haliphax@gmail.com>
2012-10-25 16:33:32 -0500

    added code to wrap Twitter links in anchors

--- www/application/controllers/login.php
+++ www/application/controllers/login.php
@@ -27,7 +27,27 @@ class Login extends CI_Controller
 
 			foreach($xml as $status)
 			{
-				$this->news .= "<li>{$status->text}<span>";
+				$this->news .= "<li>";
+				$text = $status->text;
+
+				# convert URLs into links
+				$text = preg_replace(
+					"#(https?://([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+]*)?)#i", "<a href='$1' target='_blank'>$1</a>",
+					$text);
+				# convert protocol-less URLs into links
+				$text = preg_replace(
+					"#(?!https?://|<a[^>]+>)(^|\s)(([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+.]*)?)\b#i", "$1<a href='http://$2'>$2</a>",
+					$text);
+				# convert @mentions into follow links
+				$text = preg_replace(
+					"#(?!https?://|<a[^>]+>)(^|\s)(@([_a-z0-9\-]+))#i", "$1<a href=\"{$instance['mention_url']}$3\" title=\"Follow $3\" target=\"_blank\">@$3</a>",
+					$text);
+				# convert #hashtags into tag search links
+				$text = preg_replace(
+					"#(?!https?://|<a[^>]+>)(^|\s)(\#([_a-z0-9\-]+))#i", "$1<a href='{$instance['hashtag_url']}$3' title='Search tag: $3' target='_blank'>#$3</a>",
+					$text);	
+
+				$this->news .= "{$text}<span>";
 				$this->news .= date('D M j @ g:i A', strtotime($status->created_at) + (-5 * 60));
 				$this->news .= "</span></li>";
 
