d8f5ef80fe012aadb03046734916ec0b78a2db52
Todd Boyd <haliphax@gmail.com>
2012-11-05 22:50:22 -0600

    added fault tolerance to ironworker ticks

--- tools/iron.io/tick.php
+++ tools/iron.io/tick.php
@@ -2,14 +2,30 @@
 require(dirname(__FILE__) . '/config.php');
 $payload = getPayload();
 $url = $config['url'] . $payload->tick;
-$stamp = time();
-$nonce = $stamp . "|" . base64_encode(md5($config['salt'] . $stamp));
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array("nonce" => $nonce));
-echo curl_exec($ch);
-curl_close($ch);
+$success = false;
+
+for($a = 0; $a < 5 && ! $success; $a++)
+{
+	$stamp = time();
+	$nonce = $stamp . "|" . base64_encode(md5($config['salt'] . $stamp));
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, array("nonce" => $nonce));
+	$result = curl_exec($ch);
+	$success = (strpos($result, 'Tick fired.') !== false);
+
+	if($success)
+	{
+		echo curl_exec($ch);
+	}
+	else
+	{
+		echo "cURL Failure\n";
+	}
+
+	curl_close($ch);
+}
 
