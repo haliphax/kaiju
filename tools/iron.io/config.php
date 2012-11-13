d6b4ecf44a3dd00c6e1d308d688372ad30ca2626
Todd Boyd <haliphax@gmail.com>
2012-11-13 14:53:26 -0600

    iron worker config

--- tools/iron.io/config.php
+++ tools/iron.io/config.php
@@ -1,5 +1,5 @@
 <?php
 $config = array(
-	'url' => 'http://qa-kaijugame.phpfogapp.com/index.php/tick/fire/'
+	'url' => 'http://kaiju-qa.rs.af.cm/index.php/tick/fire/'
 	, 'salt' => 'wo9Z;od1&F3ch0(QucL:2k'
 );
